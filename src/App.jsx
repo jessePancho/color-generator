890f9efa28e68bcb363cbaef6b752203246f7f4f
Jesse Pancho <juicepancho007@gmail.com>
2025-05-19 17:39:21 +0800

    update: single color

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+import Values from "values.js";
+import ColorList from "./ColorList";
+import Form from "./Form";
+import { useState } from "react";
+
 const App = () => {
-  return <h2>Color Generator Starter</h2>;
+  const [colors, setColors] = useState(new Values('#f15025').all(10));
+
+  return (
+    <main>
+      <Form />
+      <ColorList colors={colors} />
+    </main>
+  );
 };
 export default App;
---
+++ src/Form.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+const Form = () => {
+  const [color, setColor] = useState('');
+
+  const handleSubmit = (e) => { };
+
+  return (
+    <section>
+      <h4>color generator</h4>
+      <form className='color-form' onSubmit={handleSubmit}>
+        <input type='color' value={color} />
+        <input
+          type='text'
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          placeholder='#f15025'
+        />
+        <button
+          className='btn'
+          type='submit'
+          style={{ background: color }}>
+          Submit
+        </button>
+      </form>
+    </section>
+  )
+}
+
+export default Form
