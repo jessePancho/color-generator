2cd1028ad00202cdc9b523d59ef97d00b4242490
Jesse Pancho <juicepancho007@gmail.com>
2025-05-20 10:45:51 +0800

    update: done

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import Values from "values.js";
 import ColorList from "./ColorList";
 import Form from "./Form";
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   const [colors, setColors] = useState(new Values('#f15025').all(10));
 
+  const addColor = (color) => {
+    try {
+      const newColors = new Values(color).all(10);
+      setColors(newColors)
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
   return (
     <main>
-      <Form />
+      <Form addColor={addColor} />
       <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   );
 };
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
 
-const Form = () => {
+const Form = ({ addColor }) => {
   const [color, setColor] = useState('');
 
-  const handleSubmit = (e) => { };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addColor(color);
+  };
 
   return (
     <section>
       <h4>color generator</h4>
       <form className='color-form' onSubmit={handleSubmit}>
-        <input type='color' value={color} />
+        <input
+          type='color'
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
         <input
           type='text'
           value={color}
